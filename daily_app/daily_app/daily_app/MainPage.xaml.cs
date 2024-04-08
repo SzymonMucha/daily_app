11e2ae96c6479b6f0c38b268d340efc069ad2891
SzymonMucha <szymekcz148@gmail.com>
2024-04-08 22:43:31 +0200

    Few changes, writing classes for habits

--- daily_app/daily_app/daily_app/MainPage.xaml.cs
+++ daily_app/daily_app/daily_app/MainPage.xaml.cs
@@ -2,12 +2,27 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace daily_app
 {
+    public class Habit {
+        private int habitId;
+        private string habitDescription;
+        private ulong habitStreak;
+        private DateTime habitLastModDate;
+
+        //setters and getters here
+
+    }
+    public class HabitsList { 
+        public DateTime lastModDate;
+        public Habit[] HabitsCollection;
+
+    }
     public partial class MainPage : ContentPage
     {
         public MainPage()
